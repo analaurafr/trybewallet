bf0720eb2698ad7daefacb83f1ef88c8ae3db606
analaurafr <laurafernandes@outlook.com>
2023-09-23 17:39:05 -0300

    req 2 tranquilinho

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,32 @@
-// header
+import { useSelector } from 'react-redux';
+import { UserReducer } from '../types';
 
-import React from 'react';
+type UserData = {
+  user: UserReducer;
+};
 
 function Header() {
+  const data = useSelector((globalState: UserData) => ({
+    ...globalState.user,
+  }));
+
+  const { email } = data;
+
   return (
-    <div />
+    <header>
+      {/* Elemento para exibir o email da pessoa usuária */}
+      <div data-testid="email-field">
+        Email:
+        {' '}
+        { email }
+      </div>
+
+      {/* Elemento para a despesa total (inicialmente 0) */}
+      <div data-testid="total-field">Despesa Total: 0</div>
+
+      {/* Elemento para mostrar o câmbio (BRL) */}
+      <div data-testid="header-currency-field">Câmbio: BRL</div>
+    </header>
   );
 }
 
